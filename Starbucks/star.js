4e4e361556281d439ebb2d32e00c0f297fb88476
Zahir Mushtaq <77972118+xahirmushtaq@users.noreply.github.com>
2022-11-16 11:58:22 +0530

    Add files via upload
    Starbucks landing page built with HTML, CSS and JS

---
+++ Starbucks/star.js
@@ -0,0 +1,7 @@
+function ImgSlider(anything) {
+	document.querySelector(".starbucks").src = anything;
+}
+function changeCircleColor(color) {
+	const circle = document.querySelector(".circle");
+	circle.style.background = color;
+}
